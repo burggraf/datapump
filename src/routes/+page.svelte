6949bc1c98a8f72e6c45e60c6c8aefd76eca9d1a
Mark Burggraf <markb@mantisbible.com>
2024-12-24 14:28:21 -0800

    added shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
-TEST
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+
+  let count = $state(0);
+
+  function handleClick() {
+    count++;
+    alert("Button clicked!");
+  }
+</script>
+
+<Button onclick={handleClick}>Click me</Button>
+
+<p>Count: {count}</p>
