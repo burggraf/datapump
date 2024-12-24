8f727ddc1c28ae2f56a13dbeefcf86982dd46b25
Mark Burggraf <markb@mantisbible.com>
2024-12-24 15:25:46 -0800

    setup services, added papaparse

---
+++ src/lib/services/flat.svelte.ts
@@ -0,0 +1,10 @@
+export const readFile = async (file: File): Promise<string> => {
+return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+    resolve(reader.result as string);
+    };
+    reader.onerror = reject;
+    reader.readAsText(file);
+});
+};
