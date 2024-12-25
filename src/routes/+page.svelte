908a28f3525bdb717997f2c6e0950a0c8adb3046
Mark Burggraf <markb@mantisbible.com>
2024-12-24 17:09:45 -0800

    settings dialog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,14 @@
   let open = $state(false);
 </script>
 
+<header class="bg-gray-100 p-4 flex items-center justify-between">
+  <h1 class="text-2xl font-bold text-center flex-grow">Data Pump</h1>
+  <Button variant="ghost" size="icon" onclick={() => open = true}>
+    <Settings class="h-6 w-6" />
+  </Button>
+</header>
 <div class="container mx-auto p-4 flex">
-  <div class="flex justify-end">
-    <Button variant="ghost" size="icon" onclick={() => open = true}>
-      <Settings class="h-5 w-5" />
-    </Button>
-  </div>
-    <DataInput />
-    <DataOutput />
+  <DataInput />
+  <DataOutput />
   <SettingsModal bind:open={open} />
 </div>
