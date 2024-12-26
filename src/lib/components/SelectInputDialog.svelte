17161623f1f65926b579b7840e3b9e3f70f55921
Mark Burggraf <markb@mantisbible.com>
2024-12-26 08:10:40 -0800

    new UI WIP

---
+++ src/lib/components/SelectInputDialog.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import * as Dialog from "$lib/components/ui/dialog";
+	let { open = $bindable() } = $props();
+</script>
+
+<Dialog.Root {open}>
+	<Dialog.Trigger>
+		<button>Open Dialog</button>
+	</Dialog.Trigger>
+	<Dialog.Portal>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Select Input</Dialog.Title>
+			</Dialog.Header>
+		</Dialog.Content>
+	</Dialog.Portal>
+</Dialog.Root>
