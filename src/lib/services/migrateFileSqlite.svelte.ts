0040a91d5669d4e245c9999b51be3ddbc6b135e7
Mark Burggraf <markb@mantisbible.com>
2024-12-28 08:57:31 -0800

    migrateFileSqlite WIP

--- src/lib/services/migrateFileSqlite.svelte.ts
+++ src/lib/services/migrateFileSqlite.svelte.ts
@@ -11,19 +11,25 @@ export function migrate(file: File, outputConnectionString: string) {
             for (let i = 0; i < schema.length; i++) {
                 console.log(schema[i]);
             }
+            // need to get path to SQLite file and store as dbPath
+            const dbPath = outputConnectionString.replace('sqlite://', '');
 
             let sql = `CREATE TABLE IF NOT EXISTS ${tableName} (${schema.map((field) => `${field.name} ${field.type}`).join(', ')})`;
             console.log('Creating table:');
             console.log(sql);
-            /*
+            console.log("Output connection string:");
+            console.log(outputConnectionString);
             const createTableQuery = sql;
-            const { error: createTableError } = await executeSqliteQuery(outputConnectionString, createTableQuery);
+            const { error: createTableError } = await executeSqliteQuery(dbPath, createTableQuery);
             if (createTableError) {
                 reject(createTableError);
                 return;
             }
             // Placeholder for actual data insertion
-            const insertQuery = `INSERT INTO ${file.name} VALUES (${schema.map(() => '?').join(', ')})`;
+            const insertQuery = `INSERT INTO ${tableName} VALUES (${schema.map(() => '?').join(', ')})`;
+            console.log('Inserting data:');
+            console.log(insertQuery);
+            /*
             const { error: insertError } = await executeSqliteQuery(outputConnectionString, insertQuery);
             if (insertError) {
                 reject(insertError);
